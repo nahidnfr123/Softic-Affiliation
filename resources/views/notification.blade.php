d4786c8bc0a3b7a2f1e1d38610868bf11637a3f2
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-06 22:40:21 +0600

    notification.blade.php

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -9,15 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-
-                    @foreach(Auth::user()->notifications as $notification)
+                    @if(!count(Auth::user()->notifications))
                         <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 mb-4" role="alert">
-                            <p class="font-bold">{{$notification['data']['type']}}</p>
-                            <p class="text-base">{{$notification['data']['data']}}</p>
-                            <p class="text-xs"><strong>{{$notification->created_at}}</strong></p>
+                            <p class="font-bold">No Notifications available</p>
                         </div>
-                    @endforeach
-
+                    @else
+                        @foreach(Auth::user()->notifications as $notification)
+                            <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 mb-4" role="alert">
+                                <p class="font-bold">{{$notification['data']['type']}}</p>
+                                <p class="text-base">{{$notification['data']['data']}}</p>
+                                <p class="text-xs"><strong>{{$notification->created_at}}</strong></p>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
